445c65bd993a954b82d1900bf14b0ce350e3a9a7
valericio <valericio@gmail.com>
2021-01-05 22:33:40 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Captcha</title>
+</head>
+<body>
+<form action="valida.php" method="post"> Dígitos verificadores:<img src="captcha.php"> <br>
+Ingrese valor:
+<input type="text" name="numero">
+<br>
+<input type="submit" value="Verificar"> </form>
+</body>
+</html>
---
+++ valida.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+<title>Captcha</title>
+</head>
+<body>
+<?php
+if ($_SESSION['numeroaleatorio']==$_REQUEST['numero'])
+  echo "Ingresó el valor correcto";
+else
+  echo "Incorrecto";
+?>
+</body>
+</html>
